2b80e78ee74868a30560606fc8f387872673ce3a
Raymond Wu <virus.warnning@gmail.com>
2015-05-07 17:55:13 +0800

    fixed #2 檢查 amenity 異常資料 * 順便改一下 coding style

--- test_amenity.py
+++ test_amenity.py
@@ -1,12 +1,16 @@
 # coding: utf-8
 
 import osmtest
+import re
 
 class AmenityTestCase(osmtest.OsmTestCase):
 
 	## point 命名有廁所，但是 amenity 為 NULL
 	def test01_toilets_without_amenity(self):
-		sql = "SELECT osm_id,name,amenity FROM planet_osm_point WHERE name LIKE '%廁所%' AND amenity IS NULL"
+		sql = """
+			SELECT osm_id,name,amenity FROM planet_osm_point
+			WHERE name LIKE '%廁所%' AND amenity IS NULL
+		"""
 		rows = self.query(sql)
 		if len(rows)>0:
 			summary = self.get_osmid_summary(rows)
@@ -15,9 +19,36 @@ class AmenityTestCase(osmtest.OsmTestCase):
 
 	## amenity 為 toilet
 	def test02_toilets_as_toilet(self):
-		sql = "SELECT osm_id,name,amenity FROM planet_osm_point WHERE amenity LIKE '%toilet%' AND amenity!='toilets'"
+		sql = """
+			SELECT osm_id,name,amenity FROM planet_osm_point
+			WHERE amenity LIKE '%toilet%' AND amenity!='toilets'
+		"""
 		rows = self.query(sql)
 		if len(rows)>0:
 			summary = self.get_osmid_summary(rows)
 			msg = '找到 amenity 誤標記成 toilet 的廁所 (%s)' % summary
+			self.fail(msg)
+
+	## 沒命名也沒說明用途的地點 (高達 37520 個，先擱著晚點再測)
+	def test03_invalid_points(self):
+		sql = "SELECT count(*) FROM planet_osm_point WHERE amenity IS NULL AND name IS NULL"
+		pass
+
+	## amenity 疑似錯誤的地點
+	def test04_bad_amenities(self):
+		sql = '''
+			SELECT MIN(osm_id) osm_id, amenity, COUNT(*) cnt FROM planet_osm_point
+			WHERE amenity IS NOT NULL
+			GROUP BY amenity ORDER BY cnt DESC
+		'''
+		rows = self.query(sql)
+
+		pat = re.compile('^[a-z][a-z_]{0,28}[a-z]$')
+		bad_amenities = []
+		for r in rows:
+			if pat.match(r['amenity']) is None:
+				bad_amenities.append(r['amenity'])
+
+		if len(bad_amenities) > 0:
+			msg = '找到疑似錯誤的 amenity (%s)' % ', '.join(bad_amenities)
 			self.fail(msg)
