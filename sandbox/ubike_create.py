f4398461197e770f6c82ca75a4cc40d6dc898aea
Raymond Wu <virus.warnning@gmail.com>
2015-06-05 15:10:37 +0800

    refs #13 改善 changeset 訊息 (測試階段完成)

--- sandbox/ubike_create.py
+++ sandbox/ubike_create.py
@@ -19,22 +19,30 @@ user = 'virus.warnning@gmail.com'
 pwf  = '%s/.osmpass' % os.environ['HOME']
 
 # 注意!! 有中文的地方需要使用 unicode 字串型態
+chset = {
+	u'comment':    u'自動同步 U-bike 租借站',
+	u'created_by': u'小璋流同步機器人 (osmapi/0.6.0)'
+}
+
 node = {
-	'lat': 25.079681,
-	'lon': 121.575458,
-	'tag': {
-		'name':     u'捷運港墘站(2號出口)',
-		'ref':      '0122',
-		'amenity':  'bicycle_rental',
-		'brand':    u'微笑單車',
-		'operator': u'巨大機械工業股份有限公司'
+	u'lat': 25.079681,
+	u'lon': 121.575458,
+	u'tag': {
+		u'name':     u'捷運港墘站(2號出口)',
+		u'ref':      u'0122',
+		u'amenity':  u'bicycle_rental',
+		u'brand':    u'微笑單車',
+		u'operator': u'巨大機械工業股份有限公司'
 	}
 }
 
 api = OsmApi(api=host, username=user, passwordfile=pwf)
-api.ChangesetCreate()
+api.ChangesetCreate(chset)
 n = api.NodeCreate(node)
-api.ChangesetClose()
+cid = api.ChangesetClose()
 
+# 更新結果摘要
+print('Changeset 編號: %d' % cid)
+print('URL: http://api06.dev.openstreetmap.org/changeset/%d' % cid)
 print('Node 編號: %s' % n['id'])
 print('URL: http://api06.dev.openstreetmap.org/api/0.6/node/%s' % n['id'])
